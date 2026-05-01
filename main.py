0bfffe585fb5d3470b95ecbb19250b8ef3d887f9
dfof1298-prog <dfof1298@gmail.com>
2026-05-01 11:29:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -230,8 +230,8 @@ def look(cc_line):
     user = generate_user_agent()
     
     try:
-        url = 'https://www.lapovertydept.org/donate/'
-        url_ajax = 'https://www.lapovertydept.org/wordpress/wp-admin/admin-ajax.php'
+        url = 'https://www.theflorentine.net/support-the-florentine/'
+        url_ajax = 'https://www.theflorentine.net/wordpress/wp-admin/admin-ajax.php'
         
         headers = {'user-agent': user}
         r = s.get(url, headers=headers)
