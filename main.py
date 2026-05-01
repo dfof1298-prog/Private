a9fd3f53755ade28553a100c9460aac9d46ce7b9
dfof1298-prog <dfof1298@gmail.com>
2026-05-01 13:15:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -230,8 +230,8 @@ def look(cc_line):
     user = generate_user_agent()
     
     try:
-        url = 'https://beingkid.org/donations/sponsorship-opportunities-for-classes-events/'
-        url_ajax = 'https://beingkid.org/wordpress/wp-admin/admin-ajax.php'
+        url = 'https://www.dogsblog.com/donate/'
+        url_ajax = 'https://www.dogsblog.com/wordpress/wp-admin/admin-ajax.php'
         
         headers = {'user-agent': user}
         r = s.get(url, headers=headers)
